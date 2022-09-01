b3572c1f1d5f7b25447908e92981cc7a67f1ec6f
FannyHernandezMeraz <104457986+FannyHernandezMeraz@users.noreply.github.com>
2022-09-01 09:55:09 -0600

    Add files via upload

---
+++ EVA1_11_EJEMPLO_ARREGLOS/src/Arreglo_Profe.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moviles
+ */
+public class Arreglo_Profe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int arregloCali[][];
+        //Primera dimension --> grupos
+        //segunda dimension --> estudiantes por grupo
+        
+        //Determinar los grupos (PRIMERA DIMENSION)
+        arregloCali = new int[5][];
+        
+        //Para cada grupo: cuantos estudiantes necesitas?
+        arregloCali[0] = new int[10]; //Grupo 1 con 10 estudiantes
+        
+    }
+    
+}
